d36a7774362639b0620f511c46d087e9c57cc3d9
racoon0024 <r0740911@student.thomasmore.be>
2020-12-08 21:32:51 +0100

    Links homepage ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  loggedIn: boolean = false
+
+  userSub: Subscription;
+
+  constructor(private router: Router, private _authService: AuthService) { }
 
   scroll(el: HTMLElement) {
     el.scrollIntoView({behavior: 'smooth'});
   }
 
   ngOnInit(): void {
+    this.userSub = this._authService.user.subscribe((user: User) => {
+      this.loggedIn = !!user;
+    });
+  }
+
+  navigateLogin() {
+    this.router.navigate(['/login'])
+  }
+
+  navigateSignUp() {
+    this.router.navigate(['/signup'])
   }
 
 }
