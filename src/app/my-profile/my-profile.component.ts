cf8f3d83de7be03ab0491e585aac155f295fc577
racoon0024 <r0740911@student.thomasmore.be>
2020-12-08 11:24:21 +0100

    Style my profile

--- src/app/my-profile/my-profile.component.ts
+++ src/app/my-profile/my-profile.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-my-profile',
   templateUrl: './my-profile.component.html',
-  styleUrls: ['./my-profile.component.scss']
+  styleUrls: ['./my-profile.component.scss', '../styles/table_style.scss']
 })
 export class MyProfileComponent implements OnInit {
 
