fbe7746679f1cca521b5e505104ee4c7db2e74d9
robinv2006 <r0740489@student.thomasmore.be>
2020-12-10 11:18:49 +0100

    user can't delete itself

--- src/app/manage-users/manage-users.component.ts
+++ src/app/manage-users/manage-users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Observable, Subscription } from 'rxjs';
@@ -6,33 +6,38 @@ import { map } from 'rxjs/operators';
 import { User } from '../models/user.model';
 import { UserService } from '../services/user.service';
 import { ToastService } from 'src/app/toast/services/toast.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-manage-users',
   templateUrl: './manage-users.component.html',
   styleUrls: ['./manage-users.component.scss', '../styles/table_style.scss']
 })
-export class ManageUsersComponent implements OnInit {
+export class ManageUsersComponent implements OnInit, OnDestroy {
 
   users: Observable<User[]>;
   currentUser: User;
+  loggedInUser: User;
 
   pageLoaded: boolean = false;
 
   userSub: Subscription;
+  deleteUserSub: Subscription;
+  getLoggedInUserSub: Subscription;
 
   constructor(
-    private router: Router, 
-    private _modalService: NgbModal, 
+    private router: Router,
+    private _modalService: NgbModal,
     private _userService: UserService,
-    private toastService: ToastService) { }
+    private toastService: ToastService,
+    private _authService: AuthService) { }
 
   goToCreate() {
     this.router.navigate(['admin/users/create']);
   }
 
   goToEdit(user: User) {
-    this.router.navigate(['admin/users/edit'], { queryParams: { id: user.userID }});
+    this.router.navigate(['admin/users/edit'], { queryParams: { id: user.userID } });
   }
 
   openDeleteUser(user: User, contentDeleteModel) {
@@ -41,21 +46,45 @@ export class ManageUsersComponent implements OnInit {
   }
 
   deleteUser(user: User) {
-    this._userService.deleteUserById(user.userID).subscribe();
-    this.users = this.users.pipe(
-      map(res => res.filter(u => u.userID != user.userID))
-    );
-    this._modalService.dismissAll();
-    this.toastService.show('Deleted user ' + user.firstName + ' ' + user.lastName, {
-      classname: 'bg-danger text-light',
-      delay: 2000 ,
-      autohide: true
-    });
+    if (user.userID != this.loggedInUser.userID) {
+      this.deleteUserSub = this._userService.deleteUserById(user.userID).subscribe();
+      this.users = this.users.pipe(
+        map(res => res.filter(u => u.userID != user.userID))
+      );
+      this._modalService.dismissAll();
+      this.toastService.show('Deleted user ' + user.firstName + ' ' + user.lastName, {
+        classname: 'bg-danger text-light',
+        delay: 2000,
+        autohide: true
+      });
+    }
+    else {
+      console.log('deletedUser:', user);
+      console.log('loggedInUser:', this.loggedInUser);
+      this._modalService.dismissAll();
+      this.toastService.show("You can't delete yourself!", {
+        classname: 'bg-danger text-light',
+        delay: 2000,
+        autohide: true
+      });
+    }
+
   }
 
   ngOnInit(): void {
     this.users = this._userService.getUsers();
     this.userSub = this.users.subscribe(result => this.pageLoaded = true)
+    this.getLoggedInUserSub = this._authService.user.subscribe((user: User) => {
+      this.loggedInUser = user;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.deleteUserSub) {
+      this.deleteUserSub.unsubscribe();
+    }
+    this.userSub.unsubscribe();
+    this.getLoggedInUserSub.unsubscribe();
   }
 
 }
