59cb697e1ccbb424c0262f9530d0ba15905fe9f4
r0766008 <r0766008@student.thomasmore.be>
2020-12-11 16:00:26 +0100

    Choose table when challenge

--- src/app/my-profile/my-profile.module.ts
+++ src/app/my-profile/my-profile.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyProfileComponent } from './my-profile.component';
 import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [MyProfileComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ], 
   exports: [MyProfileComponent]
 })
