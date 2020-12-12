0139ae294cea66b28ecc458b18ae70337aa7c7f1
robinv2006 <r0740489@student.thomasmore.be>
2020-12-12 19:00:26 +0100

    test table sorting

--- src/app/manage-tables/manage-tables.component.ts
+++ src/app/manage-tables/manage-tables.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Directive, EventEmitter, Input, OnInit, Output, QueryList, ViewChildren } from '@angular/core';
 import { TableService } from '../services/table.service';
 import { Table } from '../models/table.model';
 import { Observable } from 'rxjs';
@@ -6,14 +6,47 @@ import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
+export type SortColumn = keyof Table | '';
+export type SortDirection = 'asc' | 'desc' | '';
+const rotate: {[key: string]: SortDirection} = { 'asc': 'desc', 'desc': '', '': 'asc' };
+
+const compare = (v1: string | number, v2: string | number) => v1 < v2 ? -1 : v1 > v2 ? 1 : 0;
+
+export interface SortEvent {
+  column: SortColumn;
+  direction: SortDirection;
+}
+
+@Directive({
+  selector: 'th[sortable]',
+  host: {
+    '[class.asc]': 'direction === "asc"',
+    '[class.desc]': 'direction === "desc"',
+    '(click)': 'rotate()'
+  }
+})
+export class NgbdSortableHeader {
+
+  @Input() sortable: SortColumn = '';
+  @Input() direction: SortDirection = '';
+  @Output() sort = new EventEmitter<SortEvent>();
+
+  rotate() {
+    this.direction = rotate[this.direction];
+    this.sort.emit({column: this.sortable, direction: this.direction});
+  }
+}
+
 @Component({
   selector: 'app-manage-tables',
   templateUrl: './manage-tables.component.html',
   styleUrls: ['./manage-tables.component.scss', '../styles/table_style.scss']
 })
+
 export class ManageTablesComponent implements OnInit {
 
   tables: Observable<Table[]>;
+  sortedTables: Table[] = [];
   tablesLength: number = 0;
   currentTable: Table;
 
@@ -41,11 +74,44 @@ export class ManageTablesComponent implements OnInit {
     this._modalService.dismissAll();
   }
 
+  sortTables(attributeSort: string) {
+    console.log('sort');
+    this.sortedTables.sort(function(a, b) {
+      if(a[attributeSort] < b[attributeSort]) return 1;
+      if(a[attributeSort] > b[attributeSort]) return -1;
+      return 0;
+    })
+  }
+
   ngOnInit(): void {
     this.tables = this._tableService.getTables();
     this.tables.subscribe(tables => {
+      this.sortedTables = tables;
       this.tablesLength = tables.length;
     });
     this.tables.subscribe(result => this.pageLoaded = true)
+  }
+
+  //sorting 
+  @ViewChildren(NgbdSortableHeader) headers: QueryList<NgbdSortableHeader>;
+
+  onSort({column, direction}: SortEvent) {
+    console.log(this.sortedTables);
+    // resetting other headers
+    this.headers.forEach(header => {
+      if (header.sortable !== column) {
+        header.direction = '';
+      }
+    });
+
+    // sorting countries
+    if (direction === '' || column === '') {
+      this.sortedTables;
+    } else {
+      this.sortedTables = [...this.sortedTables].sort((a, b) => {
+        const res = compare(a[column], b[column]);
+        return direction === 'asc' ? res : -res;
+      });
+    }
   }
 }
